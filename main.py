fae18d1b12734e6058dbc0ac8a45b6223b6934bf
Tsunami014 (Max) <Tsunami014@duck.com>
2024-11-04 17:14:04 +1100

    Made some improvements

--- main.py
+++ main.py
@@ -26,12 +26,16 @@ def printWorld(blocks, pos, angle):
     for x in range(DISPLAYSIZE[0]):
         endPos = colls.rotate(pos, (pos[0], pos[1]-VIEWDIST), angle+((x - DISPLAYSIZE[0]/2)*(FOV/DISPLAYSIZE[0])))
         ray = colls.Line(pos, endPos)
-        wheres = ray.whereCollides(blocks)
+        cs = [i for i in blocks if i.collides(ray)]
+        wheres = [j for i in cs for j in ray.whereCollides(i)]
         if len(wheres) != 0:
             wheres.sort(key=lambda p: (p[0]-pos[0])**2 + (p[1]-pos[1])**2)
             p = wheres[0]
             dist = math.sqrt((p[0]-pos[0])**2 + (p[1]-pos[1])**2)
-            c = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'."[int((dist/VIEWDIST)*50)]
+            #c = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'."[int((dist/VIEWDIST)*50)]
+            d = (p[0]-pos[0], p[1]-pos[1])
+            ts = [i.tangent(colls.Point(*p), d) for i in cs]
+            c = {0: ".", 45: ",", 90: ":", 135: ";", 180: "'", 180+45: '"', 270: "[", 270+45: "]"}[ts[0]%360]
             diff = int((dist/VIEWDIST) * (DISPLAYSIZE[1]/2))
             for y in range(DISPLAYSIZE[1]-diff*2):
                 grid[y+diff][x] = c
@@ -55,7 +59,7 @@ blocks, pos = renderTilemap("""
 
 print("\n"*DISPLAYSIZE[1], end='')
 while True:
-    print('\033[%iA'%DISPLAYSIZE[1], end='')
+    print('\033[%iA'%(DISPLAYSIZE[1]+1), end='')
     printWorld(blocks, pos, angle)
     o = readchar.readkey()
     move = [0, 0]
@@ -67,8 +71,11 @@ while True:
         move[0] -= 0.1
     if o == 'd':
         move[0] += 0.1
+    opos = pos.copy()
     pos[0] += move[0]*math.cos(math.radians(angle)) - move[1]*math.sin(math.radians(angle))
     pos[1] += move[0]*math.sin(math.radians(angle)) + move[1]*math.cos(math.radians(angle))
+    if colls.Line(opos, pos).collides(blocks):
+        pos = opos
     if o == 'q' or o == readchar.key.LEFT:
         angle -= 5
     if o == 'e' or o == readchar.key.RIGHT:
