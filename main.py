418b5d24c4e3d774a19f3225858114fc72d95730
Tsunami014 (Max) <Tsunami014@duck.com>
2024-11-05 19:30:24 +1100

    Added extra rows, fixed a buncha stuff

--- main.py
+++ main.py
@@ -2,32 +2,31 @@ import math
 import readchar
 import BlazeSudio.collisions as colls
 
+# TODO: Speed up
+# TODO: Coin animations
+
 TILESIZE = 100
 DISPLAYSIZE = (60, 30)
 VIEWDIST = 500
 FOV = 100
 
 IMGS = {
-2: """
-  #####  
- #######
-#########
-#########
-#########
-#########
- #######
-  #####
-""",
-1: """
-  #
- ##
-# #
-  #
-#####
+1: r"""
+  /---\
+ /#####\
+/###*###\
+|#*****#|
+|##***##|
+\#*###*#/
+ \#####/
+  \---/
 """
 }
 IMGSZES = {
-    1: 0.5
+    1: 0.25
+}
+IMGCOLS = {
+    1: "\033[38;5;178m"
 }
 IMGS = {j: [i.rstrip() for i in IMGS[j].split('\n') if i.strip()] for j in IMGS}
 MimgLs = {j: max(len(i) for i in IMGS[j]) for j in IMGS}
@@ -44,12 +43,29 @@ def renderTilemap(map):
             if val == '#':
                 out.append((colls.Rect(row*TILESIZE, col*TILESIZE, TILESIZE, TILESIZE, 0), 0))
             if val in renders.keys():
-                out.append((colls.Circle(row*TILESIZE, col*TILESIZE, TILESIZE, 0), renders[val]))
+                out.append((colls.Circle((row+0.5)*TILESIZE, (col+0.5)*TILESIZE, TILESIZE, 0), renders[val]))
             elif val == 'S':
                 start = [row+0.5, col+0.5]
     return out, start # TODO: colls.ShapeCombiner.combineRects
 
-EXTRAROWS = 1
+COINS = 0
+
+def handleExtras(blocks, pos):
+    pobj = colls.Point(pos[0]*TILESIZE, pos[1]*TILESIZE)
+    for obj, typ in blocks.copy():
+        if typ == 1:
+            if obj.collides(pobj):
+                global COINS
+                COINS += 1
+                blocks.remove((obj, typ))
+    
+    return blocks
+
+def printExtras():
+    return [
+        "COINS: %i"%COINS
+    ]
+
 def printWorld(blocks, pos, angle):
     pos = (pos[0] * TILESIZE, pos[1] * TILESIZE)
     def mutateShp(shape, typ):
@@ -78,7 +94,7 @@ def printWorld(blocks, pos, angle):
                 for p, shp, typ, d in wheres:
                     if typ == 0:
                         dist = math.sqrt(d)
-                        chrs = "@#$B&8%Z0OUCL/\\|{}[]()?-+~<>i!;:\"',."
+                        chrs = "@#B&8$%Z0OUCL/\\|{}[]()?-+~<>i!;:\"',."
                         c = chrs[int((dist/VIEWDIST)*len(chrs))]
                         diff = int((dist/VIEWDIST) * (DISPLAYSIZE[1]/2))
                         for y in range(DISPLAYSIZE[1]-diff*2):
@@ -90,6 +106,7 @@ def printWorld(blocks, pos, angle):
                             continue
                         IMG = IMGS[typ]
                         SZE = IMGSZES[typ]
+                        COL = IMGCOLS[typ]
                         a = (dy * (p[1] - shp.p1[1]) + dx * (p[0] - shp.p1[0])) / det
                         a = int(len(IMG[0]) * (1-a))
                         dist = math.sqrt(d)
@@ -98,57 +115,51 @@ def printWorld(blocks, pos, angle):
                         diff = int((DISPLAYSIZE[1]-hei)/2)
                         diff2 = len(IMG)/hei
                         for y in range(hei):
-                            c = IMG[int(y*diff2)][a]
-                            if c not in ("", " "):
+                            c = COL + IMG[int(y*diff2)][a] + "\033[0m"
+                            if c[c.index('m')+1:-4] not in ("", " "):
                                 grid[y+diff][x] = c
+                        diff = int((dist/VIEWDIST) * (DISPLAYSIZE[1]/2))
+                        grid[DISPLAYSIZE[1]-diff*2+diff][x] = "_"
+
     print("\033[0;0H", end="")
 
-    print("Hello!\033[K")
+    extras = printExtras()
+    print("\n".join(i+"\033[K" for i in extras))
 
-    print("\n\033[%iD"%DISPLAYSIZE[0], end='')
     for col in grid:
         print("".join(col)+"\033[K")
     
     print()
+    print('\033[%iA'%(DISPLAYSIZE[1]+2+len(extras)), end='')
 
 angle = -90
 
 blocks, pos = renderTilemap("""
-##########
-#        #
-#    C   #
-#       ##
-#   S  # #
-#        #
-#      # #
-##########
+###################
+##   # C   #      #
+# S ###  #    ##  #
+#  C #  #####     #
+###    # ##  #    #
+#C# C#   #  C # # #
+#  # # #          #
+###################
 """)
 
 # blocks, pos = renderTilemap("""
-# ####################
-# ##   #   #         #
-# #   ###  #       # #
-# #    #  ##     #   #
-# ###    # # #  #    #
-# # #  #   #       # #
-# #S                 #
-# ####################
+# #################################
+# ##   #   #       ####  #####    #
+# #   ###  #       #     ###      #
+# #    #  ##     # #    C #  #    #
+# ###    # # #  #  # # #    # #   #
+# # #  #   #       #       #      #
+# #S                              #
+# #################################
 # """)
 
-# blocks, pos = renderTilemap("""
-# ##########
-# ##   #   #
-# # S ###  #
-# #    #  ##
-# ###    # #
-# # #  #   #
-# #  # # # #
-# ##########
-# """)
 print("\033[2J", end="")
 while True:
+    blocks = handleExtras(blocks, pos)
     printWorld(blocks, pos, angle)
-    print('\033[%iA'%(DISPLAYSIZE[1]+2+EXTRAROWS), end='')
     o = readchar.readkey()
     move = [0, 0]
     if o == 'w' or o == readchar.key.UP:
@@ -162,7 +173,7 @@ while True:
     opos = pos.copy()
     pos[0] += move[0]*math.cos(math.radians(angle)) - move[1]*math.sin(math.radians(angle))
     pos[1] += move[0]*math.sin(math.radians(angle)) + move[1]*math.cos(math.radians(angle))
-    if colls.Line(opos, pos).collides([i[0] for i in blocks if i[1] == 0]):
+    if colls.Line((opos[0]*TILESIZE, opos[1]*TILESIZE), (pos[0]*TILESIZE, pos[1]*TILESIZE)).collides([i[0] for i in blocks if i[1] == 0]):
         pos = opos
     if o == 'q' or o == readchar.key.LEFT:
         angle -= 5
